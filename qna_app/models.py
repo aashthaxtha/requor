dc8a622f3ae9b433b3505b5c45534fd66ac1ef86
Aashtha <aashthaxtha@gmail.com>
2019-12-11 08:53:09 +0545

    models created for questions and answers

--- qna_app/models.py
+++ qna_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class QuestionModel(models.Model):
+    title = models.CharField(max_length=255)
+    posted_by = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    qn_desc = models.TextField(max_length=255)
+    qn_image = models.ImageField(upload_to='QuestionImage')
+
+
+class AnswerModel(models.Model):
+    answered_by = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    ans_desc = models.TextField(max_length=255)
+    ans_image = models.ImageField(upload_to='AnswerImage')
+    is_accept = models.BooleanField()
+    votes = models.IntegerField()
+    question = models.ForeignKey(QuestionModel,on_delete=models.CASCADE)
+
+
+
--- qna_app/urls.py
+++ qna_app/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path,include
+from .views import qna_app,user_app
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('qna/',include('qna_app.urls')),
-    path('user/',include('user_app.urls'))
+   
+    path('add')
 ]
